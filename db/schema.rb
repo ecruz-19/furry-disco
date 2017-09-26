433677aa33b45e60d1f4b376d1f57a0485bba372
Elizabeth <celly1684@gmail.com>
2017-09-26 18:16:49 +0000

    my final toy application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913031009) do
+ActiveRecord::Schema.define(version: 20170926173409) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string "content"
+    t.string "text"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
