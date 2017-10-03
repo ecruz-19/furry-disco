5851168825940664326cd7168472742dd51fe292
Elizabeth <celly1684@gmail.com>
2017-10-03 18:52:36 +0000

    Let there be color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
- 
+ http_basic_authenticate_with name: "boss", password: "test"
     def hello
     render html: "hello, world!"
     end
